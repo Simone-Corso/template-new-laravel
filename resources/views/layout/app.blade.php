a4fd6ab8d6d56895feb24dd8f6c746e26539ca00
Simone-Corso <scorso26@gmail.com>
2024-02-02 16:37:48 +0100

    ho aggiornato il template php per farlo funzionare in modo corretto

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Laravel')</title>
+    @vite('resources/js/app.js', 'vendor/courier/build')
+
+    @yield('head-content')
+</head>
+<body>
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')  
+    </main>
+
+    @include('partials.footer')
+
+    @yield('script-content')
+</body>
+</html>
